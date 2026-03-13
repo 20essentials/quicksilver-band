61571163e992c36f015f3dd6a5c7b4e45626ed75
DominicDev <s0l0estudi4r@gmail.com>
2026-03-13 17:37:03 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'QUICKSILVER';
+export let nameSongs = [
+	"PRIDE OF MAN", /*1*/
+	"FRESH AIR", /*2*/
+	"WHO DO YOU LOVE", /*3*/
+	"MONA", /*4*/
+	"CALVARY", /*5*/
+	"THE FOOL", /*6*/
+	"MOJO", /*7*/
+	"SHADY GROVE", /*8*/
+	"GOLD AND SILVER", /*9*/
+	"TOO LONG", /*10*/
+	"LIGHT YOUR WINDOWS", /*11*/
+	"WHAT ABOUT ME", /*12*/
+	"HAPPY TRAILS", /*13*/
+	"DINO'S SONG", /*14*/
+	"QUICKSILVER MESSENGER SERVICE", /*15*/
+	"THE TRUTH", /*16*/
+	"DON'T CRY MY LADY LOVE", /*17*/
+	"I FOUND LOVE", /*18*/
+	"HOPE", /*19*/
+	"GYPSY LIGHT", /*20*/
+]
